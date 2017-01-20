32f25fe07f2606ef524a4b2da87efe9379365331
Bhosle <442268@CTS.COM>
2017-01-20 16:20:36 +0530

    first commit1

---
+++ src/com/ws/soap/SimpleSOAPServiceImpl.java
@@ -0,0 +1,40 @@
+package com.ws.soap;
+
+import java.util.Random;
+
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+@WebService
+public class SimpleSOAPServiceImpl {
+
+	 public String sayHello(@WebParam(name="echo") String echo) {
+	        
+//	            Thread.sleep(1000*60*5); // Sleep for 5 minutes
+//	        	Thread.sleep(1000*60*1);
+		 final String s = echo;
+        	Thread thread = new Thread(){
+        	    public void run(){
+        	    	try {
+        	    		///Service1
+	        	    		Thread.sleep(500*60*1);
+		        	      System.out.println("Request For " + s);
+		        	      String res = test("Response For " + s + " Thread Running");
+		        	     //Client2
+		        	      //call Service2 method
+		        	      System.out.println(res);
+        	    	}catch(InterruptedException ex) {
+        	          ex.printStackTrace();
+        	        }
+        	    }
+        	};
+        	thread.start(); 
+	        
+	        return "Hello "+ echo;
+	    }
+	 
+	 private String test(String str){
+		 return str + new Random().nextInt();
+	 }
+	
+}
